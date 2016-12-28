dde10b284cc796e2bb36cabcdfdb80b1e8f81008
AbithaDhanavath <14wh1a0562@bvrithyderabad.edu.in>
2016-12-28 21:41:16 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-"""example URL Configuration
+"""myproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -13,9 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+from django.conf.urls import include, url
+from hello.views import myfunction
+
+ 
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+ 
+urlpatterns = (
+    url(r'hello/$',myfunction),
+    # Examples:
+    # url(r'^$', 'sample.views.home', name='home'),
+    # url(r'^sample/', include('sample.foo.urls')),
+ 
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+ 
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+   
+)
--- views.py
+++ views.py
@@ -1,19 +1,6 @@
 from django.shortcuts import render
 
-from forms import Signup_form
-
-def signup(request):
-    form = Signup_form()
-
-    name = request.POST.get('name','')
-    email = request.POST.get('email', '')
-    pass1 = request.POST.get('pass1', '')
-    pass2 = request.POST.get('pass2', '')
-
-    # Do some validations here
-
-    user = User.objects.create_user(name, email, pass2)
-    if user:
-        user.save()
-
-    return render(request, 'signup.html', {'form': form})
+# Create your views here.
+from django.http import HttpResponse
+def myfunction(request,):
+    return HttpResponse("Hello!This is our sample django application.")
